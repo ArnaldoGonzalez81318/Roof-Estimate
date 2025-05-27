694e67115546d6c36756feaba3229cbb191cacf6
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-05-27 16:19:05 -0400

    Create Calculator.jsx

---
+++ src/routes/Calculator.jsx
@@ -0,0 +1,12 @@
+import Estimator from '../components/Estimator';
+
+function Calculator() {
+  return (
+    <div>
+      <h1>Roof Estimate Calculator</h1>
+      <Estimator />
+    </div>
+  );
+}
+
+export default Calculator;
