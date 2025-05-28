2dbcacd0ab30c3a55c6020a1c0b6f9509d052693
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-05-28 16:49:08 -0400

    Delete ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++
@@ -1,9 +0,0 @@
-function ThemeToggle({ toggleTheme, theme }) {
-  return (
-    <button onClick={toggleTheme} class="theme-toggle">
-      Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
-    </button>
-  );
-}
-
-export default ThemeToggle;
