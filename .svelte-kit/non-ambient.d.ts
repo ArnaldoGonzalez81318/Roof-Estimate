b9dc4ed7d7720f13489ee818f67430dd83755044
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-05-29 09:03:20 -0400

    Update non-ambient.d.ts

--- .svelte-kit/non-ambient.d.ts
+++ .svelte-kit/non-ambient.d.ts
@@ -7,19 +7,19 @@ declare module "svelte/elements" {
 		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
 		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
 		'data-sveltekit-preload-code'?:
-			| true
-			| ''
-			| 'eager'
-			| 'viewport'
-			| 'hover'
-			| 'tap'
-			| 'off'
-			| undefined
-			| null;
+		| true
+		| ''
+		| 'eager'
+		| 'viewport'
+		| 'hover'
+		| 'tap'
+		| 'off'
+		| undefined
+		| null;
 		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
 		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
 		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
 	}
 }
 
-export {};
+export { };
