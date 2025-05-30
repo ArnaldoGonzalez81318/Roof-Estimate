c7153b933aea3dcd9016887baaf7205825b469c0
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-05-29 23:55:42 -0400

    Create index.tsx

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main class="p-8 text-center">
+        <h2 class="text-4xl font-bold mb-4">Welcome to Roofing KING Construction</h2>
+        <p class="text-lg">Your trusted partner for reliable and affordable roofing solutions.</p>
+      </main>
+    </>
+  );
+}
