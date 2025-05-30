4f4cee2eeca7ce436af1ab6ca51bd9f644c9cf24
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-05-30 14:37:51 -0400

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer class="site-footer">
+      <p>© {new Date().getFullYear()} Roofing KING Construction, Inc. All rights reserved.</p>
+      <p>
+        <a href="https://www.roofingking.com/privacy-policy">Privacy Policy</a> |
+        <a href="https://www.roofingking.com/terms-of-service"> Terms of Service</a>
+      </p>
+    </footer>
+  );
+}
