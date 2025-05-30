94910c4987f88142489868118da343df0166ff7a
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-05-30 14:37:47 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import type { JSX } from 'solid-js/jsx-runtime';
-import { ThemeProvider } from './theme';
+import type { JSX } from 'solid-js';
 
 export default function App(props: { children: JSX.Element }) {
-  return (
-    <ThemeProvider>
-      {props.children}
-    </ThemeProvider>
-  );
+  return props.children;
 }
