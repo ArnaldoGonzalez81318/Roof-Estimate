3fdea2ed42c73e8c1ad4383aae539faf431e5a3a
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-05-30 14:38:03 -0400

    Update ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -3,7 +3,7 @@ import { useTheme } from '../theme';
 export default function ThemeToggle() {
   const { toggleTheme } = useTheme();
   return (
-    <button class="px-4 py-2 bg-gray-300 dark:bg-gray-700 rounded" onClick={toggleTheme}>
+    <button class="theme-toggle" onClick={toggleTheme}>
       Toggle Theme
     </button>
   );
