fbacd30638379c5c63859cbe381bb0329858b2f6
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-06-02 23:25:03 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import { render } from 'solid-js/web';
 import { Router, Route } from '@solidjs/router';
-import { ThemeProvider } from './theme'; // ✅ import ThemeProvider here
-import App from './App';
+import { ThemeProvider } from './theme';
 import Home from './routes/index';
 import Estimate from './routes/estimate';
 import About from './routes/about';
 import './App.css';
+import './index.css';
 
 render(() => (
   <ThemeProvider>
     <Router>
-      <App>
-        <Route path="/" component={Home} />
-        <Route path="/estimate" component={Estimate} />
-        <Route path="/about" component={About} />
-      </App>
+      <Route path="/" component={Home} />
+      <Route path="/estimate" component={Estimate} />
+      <Route path="/about" component={About} />
     </Router>
   </ThemeProvider>
 ), document.getElementById('root')!);
